433a5ffe24ec17e2d71a163dc6dc873b0f3eed72
Haotian <tedsunnyday@gmail.com>
2014-02-02 20:26:18 -0500

    with flask

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+""" main.py is the top level script.
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('just a placeholder page for HOUT')
+Return "Hello World" at the root URL.
+"""
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+import os
+import sys
+
+# sys.path includes 'server/lib' due to appengine_config.py
+from flask import Flask
+from flask import render_template
+app = Flask(__name__.split('.')[0])
+
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+  """ Return hello template at application root URL."""
+  return render_template('hello.html', name=name)
+
+
