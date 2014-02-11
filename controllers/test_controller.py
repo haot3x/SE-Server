34cde25996cdc077fe5aa9a7688cae8af3608f7a
Haotian <tedsunnyday@gmail.com>
2014-02-11 14:14:21 -0500

    mongo demo CURD all done

--- controllers/test_controller.py
+++ controllers/test_controller.py
@@ -45,42 +45,56 @@ def test_random():
     y = random.randint(0,1000)
     return '{"a":%s, "b":%s}' % (x,y)
 
+
 @test_api.route("/api/tests/mongo_demo", methods=['GET'])
 def test_mongo_demo():
     return render_template('mongo_test.html',models=[])
 
-@test_api.route("/api/tests/mongo/ajax/<_id>", methods=['GET','POST'])
-def test_mongo_id(_id = None):
-    if(request.method == 'GET'):
-        doc = MongoTestModel.objects.get(id=_id)
-        return json_util.dumps(doc.to_mongo())
-    if(request.method == 'POST'):
-        if(request.json['_method'] == 'POST'):
-            pass
-        elif(request.json['_method'] == 'DELETE'):
-            doc = MongoTestModel.objects.remove(id=_id)
-            print doc.k1
-            print doc.k2
-            print doc.id
-            return json_util.dumps(doc.to_mongo())
-
-@test_api.route("/api/tests/mongo/ajax", methods=['POST'])
-def test_mongo_ajax():
+@test_api.route("/api/tests/mongo/create", methods=['POST'])
+def test_mongo_post():
     print request
     if request.method == 'POST':
         k1 = request.json['k1']
         k2 = request.json['k2']
         model = MongoTestModel(k1=k1,k2=k2)
         doc = model.save()
-        print doc.k1
-        print doc.k2
-        print doc.id
+        print json_util.dumps(doc.to_mongo())
         app.logger.info(doc)
         return json_util.dumps(doc.to_mongo())
 
+@test_api.route("/api/tests/mongo", methods=['GET'])
+@test_api.route("/api/tests/mongo/<_id>", methods=['GET','POST'])
+def test_mongo_getputdelete(_id = None):
+    """ <_id> is for update/delete/get operation"""
+    if(request.method == 'GET'):
+        if _id == None:
+            doc = MongoTestModel.objects.all()
+            app.logger.info(doc)
+            return json_util.dumps([d.to_mongo() for d in doc],default=json_util.default)
+        else:
+            doc = MongoTestModel.objects.get(id=_id)
+            return json_util.dumps(doc.to_mongo())
+    if(request.method == 'POST'):
+        if(request.json['_method'] == 'DELETE'):
+            print _id;  
+            doc = MongoTestModel.objects.get(id=_id)
+            doc.delete()
+            print json_util.dumps(doc.to_mongo())
+            return json_util.dumps(doc.to_mongo())
+        elif(request.json['_method'] == 'PUT'):
+            print _id;  
+            doc = MongoTestModel.objects.get(id=_id)
+            k1 = request.json['k1']
+            k2 = request.json['k2']
+            doc.k1 = k1
+            doc.k2 = k2
+            doc.save()
+            print json_util.dumps(doc.to_mongo())
+            return json_util.dumps(doc.to_mongo())
 
-@test_api.route("/api/tests/mongo", methods=['GET','POST'])
-def test_mongo():
+@test_api.route("/api/tests/mongo_form", methods=['GET','POST'])
+def test_mongo_form():
+    """ Form version, no need """
     
     if request.method == 'GET':
         doc = MongoTestModel.objects.all()
